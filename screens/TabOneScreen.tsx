804bc30ae3a6b5858b78c2ee69b635283c06ad95
RolandoCornejo <54423436+RolandoCornejo@users.noreply.github.com>
2021-08-23 10:31:54 -0600

    changes

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,55 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  
+  const [text1,setText1]=useState('');
+  const [text2,setText2]=useState('');
+  const [resultado,setResultado]=useState('')
+
+  function sumar (text1:any,text2:any){
+    setResultado(`${text1}+${text2}=${parseFloat(text1)+parseFloat(text2)}`)
+  }
+  function restar (text1:any,text2:any){
+    setResultado(`${text1}-${text2}=${parseFloat(text1)-parseFloat(text2)}`)
+  }
+  function multiplicar (text1:any,text2:any){
+    setResultado(`${text1}*${text2}=${parseFloat(text1)*parseFloat(text2)}`)
+  }
+  function dividir (text1:any,text2:any){
+
+    (text2!='0')?setResultado(`${text1}/${text2}=${parseFloat(text1)/parseFloat(text2)}`):setResultado(`Revice los numeros ingresados`)
+
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={{padding:15,fontSize:24,backgroundColor:'orange'}}>Suma de dos numeros</Text>
+      <TextInput style={{padding:5,height:40, borderColor:'blue',borderWidth:2,marginTop:10}} placeholder='Valor 1' keyboardType='numeric' onChange={(e)=>setText1(e.nativeEvent.text)}/>
+      <TextInput style={{padding:5,height:40, borderColor:'blue',borderWidth:2,marginTop:10}} placeholder='Valor 1' keyboardType='numeric' onChange={(e)=>setText2(e.nativeEvent.text)}/>
+      <View style={{flexDirection:'row'}}>
+      <TouchableOpacity style={{height:45, padding:5 ,backgroundColor:'blue',margin:5,}} onPress={()=>sumar(text1,text2)}>
+        <Text style={{fontSize:24,color:'white',}}>Sumar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{height:45, padding:5,backgroundColor:'blue',margin:5,}} onPress={()=>restar(text1,text2)}>
+        <Text style={{fontSize:24,color:'white',}}>Restar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{height:45, padding:5,backgroundColor:'blue',margin:5,}} onPress={()=>multiplicar(text1,text2)}>
+        <Text style={{fontSize:24,color:'white',}}>Multiplicar</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={{height:45, padding:5,backgroundColor:'blue',margin:5,}} onPress={()=>dividir(text1,text2)}>
+        <Text style={{fontSize:24,color:'white',}}>Dividir</Text>
+      </TouchableOpacity>
+      </View>
+      
+      
+      <Text style={{fontSize:25,marginTop:10}}>{resultado}</Text>
     </View>
   );
 }
